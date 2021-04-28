7623dd59b1e481744fc57e445166f9903bd3a9b1
Paul Tunison <paul.tunison@kitware.com>
2021-04-28 14:56:52 -0400

    Initial pass at fixing flake8 issues and adding type annotations
    Attempting an initial pass at resolving mypy issues as they crop up,
    though I do not expect to clear them all right now due to the initial
    pass qualification.

--- xaitk_saliency/impls/saliency/logit_sal.py
+++ xaitk_saliency/impls/saliency/logit_sal.py
@@ -1,100 +1,82 @@
-from PIL import Image
-import numpy as np
-import os
-from sklearn.metrics.pairwise import euclidean_distances
-import six
-from smqtk.algorithms.relevancy_index import get_relevancy_index_impls
-from smqtk.algorithms import get_descriptor_generator_impls
-from smqtk.algorithms import SmqtkAlgorithm
-from smqtk.algorithms.saliency import SaliencyBlackbox,ImageSaliencyAugmenter
-from smqtk.representation.data_element.memory_element import DataMemoryElement
-from smqtk.utils import plugin
+import io
+import logging
+from typing import Any, Dict, Iterable, Sequence, Tuple, Type
 
-    
-class Logit_SaliencyBlackbox (SaliencyBlackbox):
+import PIL.Image
+import numpy as np
+from smqtk_core.configuration import (
+    from_config_dict,
+    to_config_dict,
+)
+from smqtk_dataprovider.impls.data_element.memory import DataMemoryElement
+from smqtk_descriptors import DescriptorElement, DescriptorGenerator
+
+from xaitk_saliency.interfaces.saliency import (
+    ImageSaliencyAugmenter,
+    SaliencyBlackbox,
+)
+from xaitk_saliency.utils.masking import generate_masked_images
+
+
+LOG = logging.getLogger(__name__)
+
+
+class LogitSaliencyBlackbox(SaliencyBlackbox):
     """
-    Logit_SaliencyBlackbox function yields some floating point scalar value
-    for a given masked base image descriptor element that signifies the 
+    This implementation yields some floating point scalar value
+    for a given masked base image descriptor element that signifies the
     difference between the confidence value of the query image and masked
     image descriptors, used by class implementations of
     'ImageSaliencyMapGenerator'.
     """
 
-    def __init__(self, pos_descriptors, neg_descriptors, rel_index, T_descr):
+    def __init__(
+        self,
+        pos_descriptors: Sequence[DescriptorElement],
+        neg_descriptors: Sequence[DescriptorElement],
+        rel_index: Any,  # "RelevancyIndex"; TODO: switch to RankRelevancy
+        T_descr: DescriptorElement
+    ):
         """
-        :param list[smqtk.representation.DescriptorMemoryElement] 
-            pos_descriptors: is a set of positive descriptors.
-        :param list[smqtk.representation.DescriptorMemoryElement]
-               neg_descriptors: is a set of negative descriptors.
-        :param rel_index: Plugin implementation of the algorithms used to 
-        generate relevance index used to rank images
-        :type: A new instance of a class implementing the
-            ``RelevancyIndex`` class.
-        :param smqtk.representation.DescriptorElement
-              T_descr: Base image feature descriptor  
+        :param pos_descriptors: is a set of positive descriptors.
+        :param neg_descriptors: is a set of negative descriptors.
+        :param rel_index:
+            Plugin implementation of the algorithms used to generate relevance
+            index used to rank images.
+        :param T_descr:
+            Base image feature descriptor
         """
-    
         self.pos_de = pos_descriptors
-        self.neg_de =  neg_descriptors
+        self.neg_de = neg_descriptors
         self.ADJs = (self.pos_de, self.neg_de)
         self.rel_index = rel_index
         self.T_descr = T_descr
-    
-    @classmethod
-    def is_usable(self):
-        """
-        Check whether this implementation is available for use.
-        Required valid presence of Ajudications and a new instance of 
-        a class implementing the ``RelevancyIndex`` class.
-        :return: Boolean determination of whether implementation is usable.
-        :rtype: bool
-        """
 
-        valid = True
-        return valid
-        
     @classmethod
-    def from_iqr_session(cls, iqrs, descr_gen, base_image): 
-        """
-        Create an ``SaliencyBlackbox`` instance from iqrs session, descriptor
-        generator and base_image.
-        :param smqtk.iqr.IqrSession iqrs:`smqtk.iqr.IqrSession` instance.
-        :param smqtk.algorithms.DescriptorGenerator descr_gen:
-            The descriptor generator used by smqtk.
-        :param PIL.Image base_image: The Base image for which we 
-           need to calculate a saliency map.
-        :return: A new instance of a class implementing the
-            ``SaliencyBlackbox`` class.
-        :rtype: SaliencyBlackbox
-        """
-
+    def from_iqr_session(
+        cls: Type["LogitSaliencyBlackbox"],
+        iqrs: Any,  # "IqrSession"
+        descr_generator: DescriptorGenerator,
+        base_image: PIL.Image.Image
+    ) -> "LogitSaliencyBlackbox":
         assert iqrs
-        pos = list(iqrs.positive_descriptors |\
-          iqrs.external_positive_descriptors)
-        neg = list(iqrs.negative_descriptors |\
-          iqrs.external_negative_descriptors)
-        rel_index = plugin.from_plugin_config\
-         (plugin.to_plugin_config(iqrs.rel_index),\
-          get_relevancy_index_impls())
-        buff = six.BytesIO()
+        pos = list(iqrs.positive_descriptors | iqrs.external_positive_descriptors)
+        neg = list(iqrs.negative_descriptors |
+                   iqrs.external_negative_descriptors)
+        # TODO: This requires the relevancy package breakout for the types used
+        #       here.
+        rel_index: Any = from_config_dict(
+            to_config_dict(iqrs.rel_index),
+            []  # RelevancyIndex.get_impls()
+        )
+        buff = io.BytesIO()
         base_image.save(buff, format="bmp")
         de = DataMemoryElement(buff.getvalue(),
                                content_type='image/bmp')
-        T_descr=descr_gen.compute_descriptor(de)
-        return Logit_SaliencyBlackbox(pos, neg, rel_index, T_descr)
+        T_descr = descr_generator.generate_one_element(de)
+        return LogitSaliencyBlackbox(pos, neg, rel_index, T_descr)
 
-    def get_config(self):
-        """
-        Returns a JSON-compliant dictionary that could be passed to 
-        the class's ``from_config`` method to produce an instance 
-        with identical configuration.
-        In the common case, this involves naming the keys of the dictionary
-        based on the initialization argument names as if it were to be passed
-        to the constructor via dictionary expansion.
-        :return: JSON type compliant configuration dictionary.
-        :rtype: dict
-        """
-        
+    def get_config(self) -> Dict[str, Any]:
         return {
             'pos_descriptors': self.pos_de,
             'neg_descriptors': self.neg_de,
@@ -102,102 +84,74 @@ class Logit_SaliencyBlackbox (SaliencyBlackbox):
             'T_descr': self.T_descr,
         }
 
-    def transform(self, descriptors):
-        """
-        Transform some descriptor element into a saliency scalar.
-        :param Iterable[smqtk.representation.DescriptorElement] 
-           descriptors:Descriptor of augmentations to get their scalar value.
-        :return: The saliency value for the given descriptor.
-        :rtype: numpy.ndarray[float]
-        """
-        
-        descriptors_list=list(descriptors)
-        rel_train_set=[single_descr for single_descr in descriptors_list]
+    def transform(self, descriptors: Iterable[DescriptorElement]) -> np.ndarray:
+        descriptors_list = list(descriptors)
+        rel_train_set = [single_descr for single_descr in descriptors_list]
         rel_train_set.append(self.T_descr)
         self.rel_index.build_index(rel_train_set)
-        RI_scores=self.rel_index.rank(*self.ADJs) 
+        RI_scores = self.rel_index.rank(*self.ADJs)
         diff = np.ones(len(descriptors_list))
         base_RI = RI_scores[rel_train_set[-1]]
         for i in range(len(descriptors_list)):
-            diff[i]= RI_scores[rel_train_set[i]] - base_RI
+            diff[i] = RI_scores[rel_train_set[i]] - base_RI
         return diff
 
 
-class Logit_ImageSaliencyAugmenter(ImageSaliencyAugmenter):
+class LogitImageSaliencyAugmenter(ImageSaliencyAugmenter):
     """
-    Robust Augmenter that yields a number of augmentations 
-    of an input image, as well as preserved-area masks,
-    used for use in saliency map generation. 
+    Robust Augmenter that yields a number of augmentations
+    of an input image, as well as preserved-area masks
+    used in saliency map generation.
+
+    :param window_size: the block window size (with value 0, other areas
+        with value 1)
+    :param stride: the sliding step
     """
 
-    def __init__(self, window_size=50, stride=20):
-        """
-        :param int window_size: the block window size 
-        (with value 0, other areas with value 1)
-        :param int stride: the sliding step
-        """
-
+    def __init__(self, window_size: int = 50, stride: int = 20):
         self.window_size = window_size
         self.stride = stride
-        self.masks = self.generate_block_masks(window_size=window_size,\
-        stride=stride)
+        self.masks = self.generate_block_masks(
+            window_size=window_size,
+            stride=stride
+        )
 
-    @classmethod
-    def is_usable(cls):
-        """
-        Check whether this implementation is available for use.
-        Required valid to be True
-        :return:
-            Boolean determination of whether this implementation is usable.
-        :rtype: bool
-        """
-        
-        valid  = True
-        return valid
-    
-    @classmethod
-    def get_config(self):
-        """
-        Return a JSON-compliant dictionary that could be passed to 
-        this class's ``from_config`` method to produce an 
-        instance with identical configuration.
-        In the common case, this involves naming the keys of the dictionary
-        based on the initialization argument names as if it were to be passed
-        to the constructor via dictionary expansion.
-        :return: JSON type compliant configuration dictionary.
-        :rtype: dict
-        """
-
+    def get_config(self) -> Dict[str, Any]:
         return {
             'window_size': self.window_size,
             'stride': self.stride,
         }
 
-    def generate_block_masks(self, window_size, stride, image_size=(224,224)):
+    def generate_block_masks(
+        self,
+        window_size: int,
+        stride: int,
+        image_size: Tuple[int, int] = (224, 224)
+    ) -> np.ndarray:
         """
-        Generates sliding window type binary masks used in augment() to 
-        mask an image. The Images are resized to 224x224 to 
-        enable re-use of masks Generating the sliding window style masks.
-        :param int window_size: the block window size 
-        (with value 0, other areas with value 1)
-        :param int stride: the sliding step
-        :param tuple image_size: the mask size which should be the 
-        same to the image size
-        :return: the sliding window style masks
-        :rtype: numpy.ndarray
-        """
+        Generates sliding window type binary masks used in augment() to
+        mask an image. The Images are resized to 224x224 to
+        enable re-use of masks.
 
+        :param window_size: the block window size (with value 0, other areas
+            with value 1)
+        :param stride: the sliding step
+        :param image_size: the mask size which should be the same to the image
+            size
+
+        :return: the sliding window style masks
+        """
         rows = np.arange(0 + stride - window_size, image_size[0], stride)
         cols = np.arange(0 + stride - window_size, image_size[1], stride)
 
         mask_num = len(rows) * len(cols)
-        self._log.debug('mask_num: {}'.format(mask_num))
-        masks = np.ones((mask_num, image_size[0], image_size[1])\
-                                              , dtype=np.float64)
+        LOG.debug('mask_num: {}'.format(mask_num))
+        masks = np.ones((mask_num, image_size[0], image_size[1]),
+                        dtype=np.float64)
         i = 0
         for r in rows:
             for c in cols:
-                if r<0:
+                if r < 0:
                     r1 = 0
                 else:
                     r1 = r
@@ -205,7 +159,7 @@ class Logit_ImageSaliencyAugmenter(ImageSaliencyAugmenter):
                     r2 = image_size[0]
                 else:
                     r2 = r + window_size
-                if c<0:
+                if c < 0:
                     c1 = 0
                 else:
                     c1 = c
@@ -219,40 +173,10 @@ class Logit_ImageSaliencyAugmenter(ImageSaliencyAugmenter):
         masks = masks.reshape(mask_shape)
         return masks
 
-    def generate_masked_imgs(self, masks, img):
-        """
-        Apply the masks onto one input image
-        :param numpy.ndarray masks: sliding window type masks in 
-        [1, Height, Weight, 1] format.
-        :param numpy.ndarray img: Original base image
-        :return: List masked images
-        :rtype: List of PIL Images 
-        """
-
-        masked_imgs = []
-        for mask in masks:
-            masked_img = np.multiply(mask, img, casting='unsafe')
-            masked_imgs.append(Image.fromarray(np.uint8(masked_img)))
-        return masked_imgs
-    
-    def augment(self, image_mat):        
-        """
-        Takes in an image matrix and returns its augmented version
-        :param numpy.ndarray image_mat:
-            Image matrix to be augmented.
-        :return: A numpy arrays of augmented image matrices as well as masks
-            that indicate the regions in the augmented images that are
-            unmodified with respect to the input image (preserved regions).
-            Returned augmented images should be in the dimension format
-            [index, height, width [,channel]] with the the same data type as
-            the input image matrix.
-            Returned masks should be in the dimension format
-            [index, height, width,channel] with the boolean data type.
-        :rtype: (PIL.Image, numpy.ndarray)
-        """
-         
-        masked_images = self.generate_masked_imgs(self.masks, image_mat)
-        return (masked_images, self.masks)
-
-SALIENCY_BLACKBOX_CLASS = Logit_SaliencyBlackbox
-IMG_SALIENCY_AUGMENTER_CLASS = Logit_ImageSaliencyAugmenter
+    def augment(
+        self,
+        image_mat: np.ndarray
+    ) -> Tuple[Sequence[PIL.Image.Image], np.ndarray]:
+        img = PIL.Image.fromarray(image_mat)
+        masked_images = generate_masked_images(self.masks, img)
+        return masked_images, self.masks
