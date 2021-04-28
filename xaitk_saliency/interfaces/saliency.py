240e6ee5990ab9251d4f4f7d932a687f9624065b
Paul Tunison <paul.tunison@kitware.com>
2021-04-28 14:56:52 -0400

    Initial pass at fixing flake8 issues and adding type annotations
    Attempting an initial pass at resolving mypy issues as they crop up,
    though I do not expect to clear them all right now due to the initial
    pass qualification.

--- xaitk_saliency/interfaces/saliency.py
+++ xaitk_saliency/interfaces/saliency.py
@@ -1,17 +1,28 @@
 import abc
+from typing import Any, Iterable, Sequence, Tuple, Type, TypeVar
 
-from smqtk.algorithms import SmqtkAlgorithm
-from smqtk.representation.data_element.memory_element import DataMemoryElement
+import numpy as np
+import PIL.Image
+from smqtk_core import Plugfigurable
+from smqtk_descriptors import DescriptorElement, DescriptorGenerator
 
 
-class SaliencyBlackbox (SmqtkAlgorithm):
+T = TypeVar("T", bound="SaliencyBlackbox")
+
+
+class SaliencyBlackbox (Plugfigurable):
     """
     Blackbox function that produces some floating point scalar value for a
     given descriptor element.
     """
 
     @classmethod
-    def from_iqr_session(cls, iqrs, descr_generator, base_elem):
+    def from_iqr_session(
+        cls: Type["SaliencyBlackbox"],
+        iqrs: Any,  # "IqrSession"
+        descr_generator: DescriptorGenerator,
+        base_image: PIL.Image.Image
+    ) -> "SaliencyBlackbox":
         """
         Create an ``SaliencyBlackbox`` instance from an
         :class:`smqtk.iqr.IqrSession` instance.
@@ -28,91 +39,97 @@ class SaliencyBlackbox (SmqtkAlgorithm):
             if a blackbox instance.
         :param descr_generator:
             Descriptor generator instance.
-        :param base_elem:
-            A :py:class:`smqtk.representation.DataElement` representing the
-            data over which a saliency map may be generated.
+        :param base_image:
+            A :py:class:`PIL.Image.Image` over which a saliency map may be
+            generated.
 
         :return: A new instance of of a class implementing the
             ``SaliencyBlackbox`` class.
-        :rtype: SaliencyBlackbox
-
         """
         raise NotImplementedError("The ``from_iqr_session`` classmethod is "
                                   "not implemented for class ``{}``."
                                   .format(cls.__name__))
 
     @abc.abstractmethod
-    def transform(self, descriptors):
+    def transform(self, descriptors: Iterable[DescriptorElement]) -> np.ndarray:
         """
         Transform some number of descriptor elements into a saliency scalar
         values.
 
-        :param collections.Iterable[smqtk.representation.DescriptorElement] descriptors:
+        :param descriptors:
             Descriptors to get the saliency values of.
 
-        :return: The saliency value for the given descriptor.
-        :rtype: numpy.ndarray[float]
+        :return: The floating-point saliency value for the given descriptor.
         """
 
 
-class ImageSaliencyAugmenter (SmqtkAlgorithm):
+class ImageSaliencyAugmenter (Plugfigurable):
     """
     Algorithm that yields a number of augmentations of an input image, as well
     as preserved-area masks, used for use in saliency map generation.
     """
 
     @abc.abstractmethod
-    def augment(self, image_mat):
+    def augment(
+        self,
+        image_mat: np.ndarray
+    ) -> Tuple[Sequence[PIL.Image.Image], np.ndarray]:
         """
-        :param numpy.ndarray image_mat:
-            Image matrix to be augmented.
+        Takes in an image matrix and returns its augmented version
 
-        :return: A numpy arrays of augmented image matrices as well as masks
-            that indicate the regions in the augmented images that are
-            unmodified with respect to the input image (preserved regions).
+        :param image_mat:
+            Image matrix to be augmented. This should be in the format
+            [height, width [,channel]].
 
-            Returned augmented images should be in the dimension format
-            [index, height, width [,channel]] with the the same data type as
-            the input image matrix.
+        :return: A sequence of augmented images as well as congruently sized
+            array of masks that indicate the regions in the augmented images
+            that are unmodified with respect to the input image (preserved
+            regions).
+
+            Returned augmented images should have the same height, width and
+            channel format as the input image matrix.
 
             Returned masks should be in the dimension format
             [index, height, width] with the boolean data type.
-        :rtype: (PIL.Image, numpy.ndarray)
         """
 
 
-class ImageSaliencyMapGenerator (SmqtkAlgorithm):
+class ImageSaliencyMapGenerator (Plugfigurable):
     """
     Interface for the method of generation of a saliency map given an image
     augmentation and blackbox algorithms.
     """
 
     @abc.abstractmethod
-    def generate(self, image_mat, augmenter, descriptor_generator,
-                 blackbox):
+    def generate(
+        self,
+        image_mat: np.ndarray,
+        augmenter: ImageSaliencyAugmenter,
+        descriptor_generator: DescriptorGenerator,
+        blackbox: SaliencyBlackbox
+    ) -> PIL.Image.Image:
         """
         Generate an image saliency heat-map matrix given a blackbox's behavior
         over the descriptions of an augmented base image.
 
-        :param numpy.ndarray image_mat:
+        :param image_mat:
             Numpy image matrix of the format [height, width [,channel]] that is
             to be augmented.
-
-        :param ImageSaliencyAugmenter augmenter:
+        :param augmenter:
             Augmentation algorithm following
             the :py:class:`ImageSaliencyAugmenter` interface.
-
-        :param smqtk.algorithms.DescriptorGenerator descriptor_generator:
+        :param descriptor_generator:
             A descriptor generation algorithm following
             the :py:class:`smqtk.algorithms.DescriptorGenerator` interface.
-
-        :param SaliencyBlackbox blackbox:
+        :param blackbox:
             Blackbox algorithm implementation following
             the :py:class:`SaliencyBlackbox` interface.
 
-        :return: A :py:class:`numpy.ndarray` matrix of the same [height, width]
+        :return: A PIL Image with the same height and width properties as the
+            input image.
+
+            :py:class:`numpy.ndarray` matrix of the same [height, width]
             shape as the input image matrix but of floating-point type within
             the range of [0,1], where areas of higher value represent more
             salient regions according to the given blackbox algorithm.
-        :rtype: numpy.ndarray[float]
         """
