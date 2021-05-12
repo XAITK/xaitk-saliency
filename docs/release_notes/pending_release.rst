71dfc47f00375c7e2111c624c830ecb06efcb274
Paul Tunison <paul.tunison@kitware.com>
2021-05-11 21:55:49 -0400

    Ammend existing interface, add other new interfaces
    Add unit tests for interface alias functionality.

--- docs/release_notes/pending_release.rst
+++ docs/release_notes/pending_release.rst
@@ -5,6 +5,10 @@ XAITK-Saliency Pending Release Notes
 Updates / New Features
 ----------------------
 
+Interfaces
+
+* Add new interfaces in accordance to the v0.1 API draft.
+
 
 Fixes
 -----
---
+++ tests/interfaces/test_perturb_image.py
@@ -0,0 +1,37 @@
+import gc
+from typing import Dict, Any, Tuple, List
+import unittest.mock as mock
+
+import PIL.Image
+import numpy as np
+
+from xaitk_saliency.interfaces.perturb_image import PerturbImage
+
+
+class StubImpl (PerturbImage):
+
+    def perturb(self, ref_image: PIL.Image.Image) -> Tuple[List[PIL.Image.Image], np.ndarray]:
+        pass
+
+    def get_config(self) -> Dict[str, Any]:
+        pass
+
+
+def teardown_module() -> None:
+    # Destroy the stub class and collect so as to remove it as an
+    # implementation of the interface
+    global StubImpl
+    del StubImpl
+    gc.collect()
+
+
+def test_call_alias() -> None:
+    """
+    Test that the __call__ instance method is an alias to invoke the perturb
+    instance method.
+    """
+    stub = StubImpl()
+    stub.perturb = mock.Mock()  # type: ignore
+    m_img = mock.Mock(spec=PIL.Image.Image)
+    stub(m_img)
+    stub.perturb.assert_called_once_with(m_img)
