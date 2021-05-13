01dbfd59899e7b960e1770b16219a85e50d4b907
Bhavan Vasu <bhavan.vasu@korr.khq.kitware.com>
2021-05-13 10:16:33 -0400

    Intial fsal impl

--- xaitk_saliency/utils/masking.py
+++ xaitk_saliency/utils/masking.py
@@ -83,3 +83,27 @@ def generate_masked_images(
         masked_img = np.multiply(mask, img, casting='unsafe')
         masked_imgs.append(PIL.Image.fromarray(np.uint8(masked_img)))
     return masked_imgs
+
+def weight_regions_by_scalar(
+    scalar_vec: np.ndarray,
+    masks: np.ndarray) -> np.ndarray:
+    """
+    Weight some binary masks reguib with its respective vector in scalar_vec.
+
+    We expect the "masks" matrices and the image to be the same height and
+    width, and be valued in the [0, 1] floating-point range. The length
+    and order of scalar_vec and masks are assumed to be the same.
+    In the mask matrix, higher values correspond to regions of the image that
+    preserved. E.g. a 0 in the mask will translate to blacking out the
+    corresponding location in the source image.
+
+    :param scalar_vec: Weights for image regions in masks.
+    :param masks: Mask array in the [nMasks, Height, Weight] shape format.
+
+    :return: A numpy array representing the weighted heatmap.
+    """
+    final_heatmap = np.empty((masks[0].shape))
+    for val, region in zip(scalar_vec, masks):
+        heatmap += (region * val)
+    normalized_heatmap = heatmap / len(scalar_vec)
+    return normalized_heatmap
