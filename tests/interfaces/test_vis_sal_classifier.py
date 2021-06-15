e93165eccf4dfcb48e96c9d0a03f55ee84a3a67d
Paul Tunison <paul.tunison@kitware.com>
2021-06-15 11:51:02 -0400

    Fix various sonar reported code smells

--- tests/interfaces/test_vis_sal_classifier.py
+++ tests/interfaces/test_vis_sal_classifier.py
@@ -14,10 +14,10 @@ class StubImpl (ImageClassifierSaliencyMapGenerator):
         perturbed_conf: np.ndarray,
         perturbed_masks: np.ndarray,
     ) -> np.ndarray:
-        pass
+        """ Stub impl """
 
     def get_config(self) -> Dict[str, Any]:
-        pass
+        """ Stub impl """
 
 
 def teardown_module() -> None:
